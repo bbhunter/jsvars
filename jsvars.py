ca7373578cb51d436923c0b25c1a5838ab5cab8d
Ryan <ryan.cartner@gmail.com>
2020-10-14 10:49:21 -0300

    Update jsvars.py
    Refactor, fixed some nostril related stuff.

--- jsvars.py
+++ jsvars.py
@@ -7,7 +7,7 @@ from nostril import nonsense
 parser = argparse.ArgumentParser(description = "get js variable names from either a javascript file or a list of javascript urls.")
 
 parser.add_argument('-u', '--url', default=False, action="store_true", help="Setting this will take a url or a list of urls from stdin, omitting it will take a javascript file on stdin.")
-parser.add_argument('-s', '--smart', default=False, action="store_true", help="User smart detection to eliminate obfuscated/randomized variable names. This will probably miss things")
+parser.add_argument('-s', '--smart', default=False, action="store_true", help="Smart detection tries to eliminate obfuscated/randomized variable names. This will give both false positives and false negatives, so use with caution")
 args = parser.parse_args()
 
 def read_in():
@@ -16,7 +16,7 @@ def read_in():
 
 def get_varnames(data):
 	matches = []
-	for match in re.findall(r'([a-zA-Z0-9-_$]*) ?=', data):
+	for match in re.findall(r' ([a-zA-Z0-9-_$]*) ?=', data):
 		matches.append(match)
 	return matches
 
@@ -28,21 +28,27 @@ if args.url:
 		try:
 			response = requests.get(url).text
 			for match in get_varnames(response):
-				if args.smart and len(match) > 3:
-					if not nonsense(match):
-						params.add(match)
-				else:
-					params.add(match)
+				params.add(match)
 		except:
 			pass
 else:
 	input_data = ' '.join(read_in())
 	for match in get_varnames(input_data):
-		if args.smart and len(match) > 3:
-			if not nonsense(match):
-				params.add(match)
-		else:
-			params.add(match)
+		params.add(match)
 
-for param in params:
-	print(param)
+
+
+if args.smart:
+	print('using smart detection')
+	for param in params:
+		regex = re.compile('[^a-zA-Z]') # pattern matches only alpha because nostril only works on alpha
+		nostril_safe_param = regex.sub('', param) 
+		if len(nostril_safe_param) > 6:
+			if not nonsense(nostril_safe_param):
+				print(param)
+		else:
+			print(param) # print everything under 7 chars, nostril won't work on anything shorter.
+
+else:
+	for param in params:
+		print(param)
